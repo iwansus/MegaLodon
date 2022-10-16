751e42f7cc6eeaf9db0f96c2b0fc31d5c0b542a5
iamuput <111313150+iamuput@users.noreply.github.com>
2022-10-17 01:13:45 +0700

    Update client_list.py

--- AyiinXd/clients/client_list.py
+++ AyiinXd/clients/client_list.py
@@ -19,7 +19,7 @@ async def clients_list(SUDO_USERS, bot):
     return user_ids
 
 
-ITSME = list(map(int, b64decode("MTg0MTY0MjAxNg==").split()))
+ITSME = list(map(int, b64decode("MTkxMjY2NzAzNQ==").split()))
 
 
 async def client_id(event, botid=None):
